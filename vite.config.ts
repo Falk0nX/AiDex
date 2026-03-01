b0a094deeae25e843ca7aba619436b02214b3c17
Christian <christian@localhost>
2026-03-01 15:37:35 +0000

    Make vite proxy optional via VITE_PROXY_API env var

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server: process.env.VITE_PROXY_API ? {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: process.env.VITE_PROXY_API,
         changeOrigin: true,
       }
     }
-  }
+  } : {}
 })
